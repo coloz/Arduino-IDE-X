a99e09c2f77f01e1be08904459062935e1988c4c
coloz <coloz999@gmail.com>
2022-11-26 04:38:09 +0800

    更新

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,5 +1,5 @@
 export const APP = {
-    name: "B4A",
+    name: "Arduino IDE X",
     website: "https://b4a.clz.me",
     updateUrl: "https://b4a.clz.me/update.json",
     boardUrl: "https://b4a.clz.me/boards.json",
